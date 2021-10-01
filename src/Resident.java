5d1c292db1c91096aa140889f2b169269fe76a31
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-01 19:57:51 -0400

    Create Resident.java

---
+++ src/Resident.java
@@ -0,0 +1,17 @@
+public class Resident extends Student {
+
+	public Resident(Profile profile, boolean isFullTime) {
+		super(profile, isFullTime);
+	}
+	
+	@Override
+	public void tuitionDue() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
