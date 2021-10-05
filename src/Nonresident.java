2158bedf096ee53b091abbd5038a405cc1d1440a
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-05 19:45:49 -0400

    Update Nonresident.java

--- src/Nonresident.java
+++ src/Nonresident.java
@@ -1,11 +1,31 @@
 public class Nonresident extends Student {
 
-	public Nonresident(Profile profile, boolean isFullTime) {
-		super(profile, isFullTime);
+	public Nonresident(Profile profile, boolean isFullTime, int creditHours) {
+		super(profile, isFullTime, creditHours);
 	}
 	
 	@Override
-	public void tuitionDue() {
+	public void tuitionDue() { //why would this return void? shouldn't it return the value of tuition?
+		//also how are we supposed to access the information of the student if there's no input??
+		
+		double tuition;
+		
+		if(!this.getIsFullTime()) {
+			tuition = (966 * this.getCreditHours()) + 2614.4;
+		}
+		
+		else {
+			tuition = 29737 + 3268;
+		
+		//create instance of student??? how to access credit hours???
+		//when calling : exampleResident.tuitionDue();
+		
+			if (this.getCreditHours() > 16) {
+				tuition += (966 * (this.getCreditHours() - 16));
+			}
+		}
+		
+		this.setTuitionDue(tuition);
 		
 	}
 	
