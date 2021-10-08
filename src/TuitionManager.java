4244740664c8af024260498e419dcecd30fd6a13
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-07 22:34:07 -0400

    Update TuitionManager.java

--- src/TuitionManager.java
+++ src/TuitionManager.java
@@ -13,71 +13,102 @@ public class TuitionManager {
 		sc.useDelimiter("\n");
 		String str = sc.nextLine();
 		
-		//StringTokenizer tokenizer = new StringTokenizer(str);
-		
 		while(sc.hasNextLine()) {
 			String input = sc.next();
-			//C, P, PN, PT have 1
-			//S
-			//T has 4: T,John Doe,CS,1000,7/13/2021
 			
-			input.replace(" ", "");
+			input = input.replace(" ", "");
 			StringTokenizer tokenizer = new StringTokenizer(input,",");
 			
 			if (tokenizer.hasMoreElements()) {
 				String command = tokenizer.nextToken();
+				String newInput = input.replaceAll(command + ",", "");
 				
 				String name;
 				String major;
-				int noCredits;
-				double tuiitionDue;
-				boolean isFullTime;
-				boolean isStudyAbroad;
-				String state;
+				//int noCredits = 0;
+				//double tuitionDue = 0;
+				//boolean isFullTime = false;
+				int tuitionDue = 0;
 				Profile profile = new Profile();
+				Student newStudent = new Student();
+				Student foundStudent = new Student();
 				
 				switch (command) {
-					case "AR": name = tokenizer.nextToken();
-							   major = tokenizer.nextToken();
-							   profile = makeNewProfile(profile, name, major);
-							   noCredits = Integer.valueOf(tokenizer.nextToken());
-							   isFullTime = (noCredits < MINFULLTIME) ? false : true;
-							   Resident newResident = new Resident(profile, isFullTime, noCredits);
+					case "AR": //name = tokenizer.nextToken();
+							   //major = tokenizer.nextToken();
+							   //profile = makeNewProfile(profile, name, major);
+							   //Resident newResident = makeNewResident(input, tokenizer, profile, noCredits, tuitionDue, isFullTime);
+							   //noCredits = Integer.valueOf(tokenizer.nextToken());
+							   //isFullTime = (noCredits < MINFULLTIME) ? false : true;
+							   //Resident newResident = new Resident(profile, isFullTime, tuitionDue, noCredits);
+							   //newResident.setTuitionDue(newResident.getTuitionDue());
+							   Resident newResident = makeNewResident(newInput, tokenizer);
 							   roster.add(newResident);
 							   
-					case "AN" : name = tokenizer.nextToken();
-					   			major = tokenizer.nextToken();
-					   			profile = makeNewProfile(profile, name, major);
-					   			noCredits = Integer.valueOf(tokenizer.nextToken());
-					   			isFullTime = (noCredits < MINFULLTIME) ? false : true;
-					   			Nonresident newNonresident = new Nonresident(profile, isFullTime, noCredits);
+					case "AN" : //name = tokenizer.nextToken();
+					   			//major = tokenizer.nextToken();
+					   			//profile = makeNewProfile(profile, name, major);
+					   			//noCredits = Integer.valueOf(tokenizer.nextToken());
+					   			//isFullTime = (noCredits < MINFULLTIME) ? false : true;
+					   		//	Nonresident newNonresident = new Nonresident(profile, isFullTime, noCredits);
+					   			Nonresident newNonresident = makeNewNonresident(newInput, tokenizer);
 					   			roster.add(newNonresident);
 					   			
-					case "AI" : name = tokenizer.nextToken();
-								major = tokenizer.nextToken();
-								profile = makeNewProfile(profile, name, major);
-								noCredits = Integer.valueOf(tokenizer.nextToken());
-								isFullTime = (noCredits < MINFULLTIME) ? false : true;
-								isStudyAbroad = Boolean.valueOf(tokenizer.nextToken());
-								International newInternational = new International(profile, isFullTime, noCredits, isStudyAbroad);
+					case "AI" : //name = tokenizer.nextToken();
+								//major = tokenizer.nextToken();
+								//profile = makeNewProfile(profile, name, major);
+								//noCredits = Integer.valueOf(tokenizer.nextToken());
+								//isFullTime = (noCredits < MINFULLTIME) ? false : true;
+								//isStudyAbroad = Boolean.valueOf(tokenizer.nextToken());
+								//International newInternational = new International(profile, isFullTime, noCredits, isStudyAbroad);
+								International newInternational = makeNewInternational(newInput, tokenizer);
 								roster.add(newInternational);
 								
-					case "AT" : name = tokenizer.nextToken();
-								major = tokenizer.nextToken();
-								profile = makeNewProfile(profile, name, major);
-								noCredits = Integer.valueOf(tokenizer.nextToken());
-								isFullTime = (noCredits < MINFULLTIME) ? false : true;
-								state = tokenizer.nextToken().toUpperCase();
-								Tristate newTristate = new Tristate(profile, isFullTime, noCredits, state);
+					case "AT" : //name = tokenizer.nextToken();
+								//major = tokenizer.nextToken();
+								//profile = makeNewProfile(profile, name, major);
+								//noCredits = Integer.valueOf(tokenizer.nextToken());
+								//isFullTime = (noCredits < MINFULLTIME) ? false : true;
+								//state = tokenizer.nextToken().toUpperCase();
+								//Tristate newTristate = new Tristate(profile, isFullTime, noCredits, state);
+								Tristate newTristate = makeNewTristate(newInput, tokenizer);
 								roster.add(newTristate);
 								
 					case "R" : name = tokenizer.nextToken();
 							   major = tokenizer.nextToken();
-							   profile = makeNewProfile(profile, name, major);
-							   Student newStudent = new Student(profile);
+							   profile = makeNewProfile(name, major);
+							   newStudent = new Student(profile);
 							   roster.remove(newStudent);
-								
-					default: throw new IllegalArgumentException();
+							   
+					case "T" : name = tokenizer.nextToken();
+							   major = tokenizer.nextToken();
+							   profile = makeNewProfile(name, major);
+							   newStudent = new Student(profile);
+							   foundStudent = roster.giveStudent(newStudent);
+							   foundStudent.setTuitionDue(foundStudent.getTuitionDue() - Integer.valueOf(tokenizer.nextToken()));
+							   //THE NEXT TOKEN IS A DATE SO MAKE SURE IT'S VALID!! 
+							   //USE LAST PROJECT TO FIGURE OUT HOW TO DO ALL THAT
+					case "C" : roster.getAllTuitions();
+							   System.out.println("Calculation Completed");
+							   
+					case "F" : name = tokenizer.nextToken();
+					   	       major = tokenizer.nextToken();
+					   	       profile = makeNewProfile(name, major);
+					   	       newStudent = new Student(profile);
+					   	       foundStudent = roster.giveStudent(newStudent);
+					   	       int reducedPrice = Integer.valueOf(tokenizer.nextToken());
+					   	       if (reducedPrice < 0 || reducedPrice > 10000) {
+					   	    	   throw new IllegalArgumentException("financial aid value invalid");
+					   	       }
+					   	       foundStudent.setTuitionDue(foundStudent.getTuitionDue() - Integer.valueOf(tokenizer.nextToken()));
+					   	       
+					case "S" : profile = makeNewProfile2(newInput, tokenizer);
+			   	       		   newStudent = new Student(profile);
+			   	       		   International foundInternational = roster.getInternational(newStudent);
+			   	       		   foundInternational.setIsStudyAbroad(true);
+			   	       		   
+			   	    //now just the prints UGH that's a tomorrow problem
+					default: throw new IllegalArgumentException("invalid command");
 							   
 				}
 				
@@ -88,11 +119,86 @@ public class TuitionManager {
 		
 	}
 	
-	public Profile makeNewProfile(Profile profile, String name, String strMajor) {
+	
+	public Resident makeNewResident(String line, StringTokenizer T) {
+		String name = T.nextToken();
+		String major = T.nextToken();
+		Profile profile = makeNewProfile(name, major);
+		int noCredits = Integer.valueOf(T.nextToken());
+		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
+		double tuitionDue = 0;
+		Resident newResident = new Resident(profile, isFullTime, noCredits, tuitionDue);
+		newResident.setTuitionDue(newResident.getTuitionDue());
+		newResident.tuitionDue();
+		return newResident;
+		
+	}
+	
+	public Nonresident makeNewNonresident(String line, StringTokenizer T) {
+		String name = T.nextToken();
+		String major = T.nextToken();
+		Profile profile = makeNewProfile(name, major);
+		int noCredits = Integer.valueOf(T.nextToken());
+		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
+		double tuitionDue = 0;
+		Nonresident newNonresident = new Nonresident(profile, isFullTime, noCredits, tuitionDue);
+		newNonresident.setTuitionDue(newNonresident.getTuitionDue());
+		newNonresident.tuitionDue();
+		return newNonresident;
+		
+	}
+	
+	public International makeNewInternational(String line, StringTokenizer T) {
+		String name = T.nextToken();
+		String major = T.nextToken();
+		Profile profile = makeNewProfile(name, major);
+		int noCredits = Integer.valueOf(T.nextToken());
+		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
+		double tuitionDue = 0;
+		boolean isStudyAbroad = Boolean.valueOf(T.nextToken());
+		International newInternational = new International(profile, isFullTime, noCredits, tuitionDue, isStudyAbroad);
+		newInternational.setTuitionDue(newInternational.getTuitionDue());
+		newInternational.tuitionDue();
+		return newInternational;
+		
+	}
+	
+	public Tristate makeNewTristate(String line, StringTokenizer T) {
+		String name = T.nextToken();
+		String major = T.nextToken();
+		Profile profile = makeNewProfile(name, major);
+		int noCredits = Integer.valueOf(T.nextToken());
+		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
+		double tuitionDue = 0;
+		String state = T.nextToken().toUpperCase();
+		//error statement for if state isn't NY or CT
+		Tristate newTristate = new Tristate(profile, isFullTime, noCredits, tuitionDue, state);
+		newTristate.setTuitionDue(newTristate.getTuitionDue());
+		newTristate.tuitionDue();
+		return newTristate;
+		
+	}
+	
+	public Profile makeNewProfile(String name, String strMajor) {
+		Profile profile = new Profile();
 		String strMajorCap = strMajor.toUpperCase();
 		Major major = Major.valueOf(strMajorCap);
 		profile.setName(name);
 		profile.setMajor(major);
 		return profile;
 	}
+	
+	public Profile makeNewProfile2(String line, StringTokenizer T) {
+		String name = T.nextToken();
+		String strMajor = T.nextToken().toUpperCase();
+		Major major = Major.valueOf(strMajor);
+		Profile profile = new Profile();
+		profile.setName(name);
+		profile.setMajor(major);
+		return profile;
+		
+	}
+	
+
+	
 }
