98a410edf777de72d53bdf848a9aedde2ffd363c
cris431 <32299513+cris431@users.noreply.github.com>
2021-10-11 02:07:13 -0400

    Update Resident.java

--- src/Resident.java
+++ src/Resident.java
@@ -1,6 +1,18 @@
-
+/**
+This class defines the type Resident, which is an extenstion of Student, with all of its attributes and methods
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public class Resident extends Student {
-
+	
+	/**
+	Constructor for a Resident student
+	Creates a type Student for this student
+	@param profile the profile to set 
+	@param isFullTime sets the student to being full time or not 
+	@param creditHours the credit hours to set
+	@param tuitionDue the tuition due to set
+	@author Emily Nelson
+	*/
 	public Resident(Profile profile, boolean isFullTime, int creditHours, double tuitionDue) {
 		super(profile, isFullTime, creditHours, tuitionDue);
 	}
@@ -11,6 +23,10 @@ public class Resident extends Student {
 	public static final int PRICE_PER_CREDIT_HOUR = 404;
 	public static final int MAX_CREDIT_NO_FEE = 16;
 	
+	/**
+	Sets the amount of tuituion due from student
+	@author Emily Nelson
+	*/
 	@Override
 	public void tuitionDue() { //why would this return void? shouldn't it return the value of tuition?
 		//also how are we supposed to access the information of the student if there's no input??
@@ -40,6 +56,11 @@ public class Resident extends Student {
 		
 	}
 	
+	/**
+	Returns the student in string form
+	@return textual represential of student 
+	@author Emily Nelson
+	*/
 	@Override
 	public String toString() {
 		return super.toString();
