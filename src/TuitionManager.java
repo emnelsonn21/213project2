9fe8c0056ebb35f4d91227c6bc104a1637e8449b
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-11 19:38:07 -0400

    Update TuitionManager.java

--- src/TuitionManager.java
+++ src/TuitionManager.java
@@ -14,13 +14,13 @@ public class TuitionManager {
 	@author Emily Nelson, Cristofer Gomez-Martinez
 	*/
 	public void run() {
+		System.out.println("Tuition Manager starts running");
 		Roster roster = new Roster();
 		Student[] newRoster = new Student[4];
 		roster.setStudents(newRoster);
 		
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\n");
-		String str = sc.nextLine();
 		
 		while(sc.hasNextLine()) {
 			String input = sc.next();
@@ -30,100 +30,126 @@ public class TuitionManager {
 			
 			if (tokenizer.hasMoreElements()) {
 				String command = tokenizer.nextToken();
+				command = command.trim();
 				String newInput = input.replaceAll(command + ",", "");
 				
 				String name;
 				String major;
-				//int noCredits = 0;
-				//double tuitionDue = 0;
-				//boolean isFullTime = false;
 				int tuitionDue = 0;
 				Profile profile = new Profile();
 				Student newStudent = new Student();
 				Student foundStudent = new Student();
 				
 				switch (command) {
-					case "AR": //name = tokenizer.nextToken();
-							   //major = tokenizer.nextToken();
-							   //profile = makeNewProfile(profile, name, major);
-							   //Resident newResident = makeNewResident(input, tokenizer, profile, noCredits, tuitionDue, isFullTime);
-							   //noCredits = Integer.valueOf(tokenizer.nextToken());
-							   //isFullTime = (noCredits < MINFULLTIME) ? false : true;
-							   //Resident newResident = new Resident(profile, isFullTime, tuitionDue, noCredits);
-							   //newResident.setTuitionDue(newResident.getTuitionDue());
-							   Resident newResident = makeNewResident(newInput, tokenizer);
-							   roster.add(newResident);
+					case "AR": Resident newResident = makeNewResident(newInput, tokenizer);
+						   if (newResident == null) {
+							break;
+					 	   }
+						   addStudent(newResident, worked, roster);
+						   break;
 							   
-					case "AN" : //name = tokenizer.nextToken();
-					   			//major = tokenizer.nextToken();
-					   			//profile = makeNewProfile(profile, name, major);
-					   			//noCredits = Integer.valueOf(tokenizer.nextToken());
-					   			//isFullTime = (noCredits < MINFULLTIME) ? false : true;
-					   		//	Nonresident newNonresident = new Nonresident(profile, isFullTime, noCredits);
-					   			Nonresident newNonresident = makeNewNonresident(newInput, tokenizer);
-					   			roster.add(newNonresident);
+					case "AN" : Nonresident newNonresident = makeNewNonresident(newInput, tokenizer);
+					   			if (newNonresident == null) {
+									   break;
+								   }
+								   addStudent(newNonresident, worked, roster);
+					   			break;
 					   			
-					case "AI" : //name = tokenizer.nextToken();
-								//major = tokenizer.nextToken();
-								//profile = makeNewProfile(profile, name, major);
-								//noCredits = Integer.valueOf(tokenizer.nextToken());
-								//isFullTime = (noCredits < MINFULLTIME) ? false : true;
-								//isStudyAbroad = Boolean.valueOf(tokenizer.nextToken());
-								//International newInternational = new International(profile, isFullTime, noCredits, isStudyAbroad);
-								International newInternational = makeNewInternational(newInput, tokenizer);
-								roster.add(newInternational);
+					case "AI" : International newInternational = makeNewInternational(newInput, tokenizer);
+								if (newInternational == null) {
+									   break;
+								   }
+								addStudent(newInternational, worked, roster);
+								break;
 								
-					case "AT" : //name = tokenizer.nextToken();
-								//major = tokenizer.nextToken();
-								//profile = makeNewProfile(profile, name, major);
-								//noCredits = Integer.valueOf(tokenizer.nextToken());
-								//isFullTime = (noCredits < MINFULLTIME) ? false : true;
-								//state = tokenizer.nextToken().toUpperCase();
-								//Tristate newTristate = new Tristate(profile, isFullTime, noCredits, state);
-								Tristate newTristate = makeNewTristate(newInput, tokenizer);
-								roster.add(newTristate);
+					case "AT" : Tristate newTristate = makeNewTristate(newInput, tokenizer);
+								if (newTristate == null) {
+									break;
+								}
+								addStudent(newTristate, worked, roster);
+								break;
 								
 					case "R" : name = tokenizer.nextToken();
-							   major = tokenizer.nextToken();
+							   major = tokenizer.nextToken().trim();
 							   profile = makeNewProfile(name, major);
 							   newStudent = new Student(profile);
-							   roster.remove(newStudent);
+							   worked = roster.remove(newStudent);
+							   if (worked == true) {
+								   System.out.println("Student removed");
+							   } else {
+								   System.out.println("Student not found");
+							   }
+							   break;
 							   
 					case "T" : name = tokenizer.nextToken();
-							   major = tokenizer.nextToken();
+							   major= tokenizer.nextToken();
+							   Double amtPaid = Double.valueOf(tokenizer.nextToken());
+							   Date newDate = new Date(tokenizer.nextToken().trim());
+							   if (!newDate.isValid()) {
+								   System.out.println("Invalid date");
+							   }
 							   profile = makeNewProfile(name, major);
 							   newStudent = new Student(profile);
 							   foundStudent = roster.giveStudent(newStudent);
-							   foundStudent.setTuitionDue(foundStudent.getTuitionDue() - Integer.valueOf(tokenizer.nextToken()));
-							   //THE NEXT TOKEN IS A DATE SO MAKE SURE IT'S VALID!! 
-							   //USE LAST PROJECT TO FIGURE OUT HOW TO DO ALL THAT
+							   if (foundStudent.getTuitionDue() >= amtPaid) {
+								   foundStudent.setTuitionDue(foundStudent.getTuitionDue() - amtPaid);
+							   } else {
+								   System.out.println("Payment exceeds tuition due.");
+							   }
+							   foundStudent.setDatePaid(newDate);
+							   System.out.println("Tuition updated");
+							   break;
+						
 					case "C" : roster.getAllTuitions();
 							   System.out.println("Calculation Completed");
+							   break;
 							   
 					case "F" : name = tokenizer.nextToken();
 					   	       major = tokenizer.nextToken();
 					   	       profile = makeNewProfile(name, major);
 					   	       newStudent = new Student(profile);
 					   	       foundStudent = roster.giveStudent(newStudent);
-					   	       int reducedPrice = Integer.valueOf(tokenizer.nextToken());
-					   	       if (reducedPrice < 0 || reducedPrice > 10000) {
-					   	    	   throw new IllegalArgumentException("financial aid value invalid");
+					   	       if (!(foundStudent instanceof Resident)) {
+					   	    	   System.out.println("Non-resident student doesn't qualify for the award.");
+					   	    	   break;
 					   	       }
-					   	       foundStudent.setTuitionDue(foundStudent.getTuitionDue() - Integer.valueOf(tokenizer.nextToken()));
+					   	       if (!foundStudent.getIsFullTime()) {
+					   	    	   System.out.println("Parttime student doesn't qualify for the award.");
+					   	    	   break;
+					   	       }
+					   	       if (foundStudent.getDidFinancialAid() != 0) {
+					   	    	   System.out.println("Awarded once already.");
+					   	    	   break;
+					   	       }
+					   	       double discountedPrice = Integer.valueOf(tokenizer.nextToken().trim());
+					   	       if (discountedPrice < 0 || discountedPrice > 10000) {
+					   	    	  System.out.println("financial aid value invalid.");
+					   	       }
+					   	       foundStudent.setTuitionDue(foundStudent.getTuitionDue() - discountedPrice);
+					   	       foundStudent.setDidFinancialAid(discountedPrice);
+					   	       System.out.println("Financial aid applied.");
+					   	       break;
 					   	       
-					case "S" : profile = makeNewProfile2(newInput, tokenizer);
+					case "S" : profile = makeNewProfileFromCommand(newInput, tokenizer);
 			   	       		   newStudent = new Student(profile);
 			   	       		   International foundInternational = roster.getInternational(newStudent);
-			   	       		   foundInternational.setIsStudyAbroad(true);
-						
-					case "P" : roster.print();
-					
-					case "PN" : roster.printByName();
-								
-					case "PT" : roster.printByPaymentDate();
+			   	       		   if (foundInternational != null) {
+			   	       			   foundInternational.setIsStudyAbroad(true);
+			   	       		   } else {
+			   	       			   System.out.println("International Student not found");
+			   	       		   }
+			   	       		   break;
 			   	       		   
-			   	    //now just the prints UGH that's a tomorrow problem
-					default: throw new IllegalArgumentException("invalid command");
+					case "P" : roster.print();
+							   break;
+							   
+					case "PT" : roster.printByPaymentDate();
+					  			break;
+					  			
+					case "PN" : roster.printByName();
+							    break;
+
+					default: System.out.println("Command " + command + " not supported!");
 							   
 				}
 				
@@ -142,10 +168,55 @@ public class TuitionManager {
 	@author Emily Nelson
 	*/
 	public Resident makeNewResident(String line, StringTokenizer T) {
-		String name = T.nextToken();
-		String major = T.nextToken();
-		Profile profile = makeNewProfile(name, major);
-		int noCredits = Integer.valueOf(T.nextToken());
+		String name= null;
+		String major = null;
+		Profile profile = new Profile();
+		
+		try {
+			name = T.nextToken();
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		try {
+			major = T.nextToken().trim();
+			profile = makeNewProfile(name, major);
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+			
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		
+		int noCredits = 0;
+		try {
+			noCredits = Integer.valueOf(T.nextToken().trim());
+		} 
+		catch (NumberFormatException nfe){
+			System.out.println("invalid credit hours");
+			return null;
+		} catch (NoSuchElementException nse){
+			System.out.println("Credit hours missing.");
+			return null;
+		}
+		if (noCredits < 0) {
+			System.out.println("Credit hours cannot be negative.");
+		}
+		if (noCredits < 3) {
+			System.out.println("Minimum credit hours is 3.");
+			return null;
+		}
+		if (noCredits > 24) {
+			System.out.println("Credit hours exceed the maximum 24.");
+			return null;
+		}
 		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
 		double tuitionDue = 0;
 		Resident newResident = new Resident(profile, isFullTime, noCredits, tuitionDue);
@@ -163,10 +234,55 @@ public class TuitionManager {
 	@author Emily Nelson
 	*/
 	public Nonresident makeNewNonresident(String line, StringTokenizer T) {
-		String name = T.nextToken();
-		String major = T.nextToken();
-		Profile profile = makeNewProfile(name, major);
-		int noCredits = Integer.valueOf(T.nextToken());
+		String name= null;
+		String major = null;
+		Profile profile = new Profile();
+		
+		try {
+			name = T.nextToken();
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		try {
+			major = T.nextToken().trim();
+			profile = makeNewProfile(name, major);
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+			
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		
+		int noCredits = 0;
+		try {
+			noCredits = Integer.valueOf(T.nextToken().trim());
+		} 
+		catch (NumberFormatException nfe){
+			System.out.println("invalid credit hours");
+			return null;
+		} catch (NoSuchElementException nse){
+			System.out.println("Credit hours missing.");
+			return null;
+		}
+		if (noCredits < 0) {
+			System.out.println("Credit hours cannot be negative.");
+		}
+		if (noCredits < 3) {
+			System.out.println("Minimum credit hours is 3.");
+			return null;
+		}
+		if (noCredits > 24) {
+			System.out.println("Credit hours exceed the maximum 24.");
+			return null;
+		}
 		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
 		double tuitionDue = 0;
 		Nonresident newNonresident = new Nonresident(profile, isFullTime, noCredits, tuitionDue);
@@ -184,13 +300,66 @@ public class TuitionManager {
 	@author Emily Nelson
 	*/
 	public International makeNewInternational(String line, StringTokenizer T) {
-		String name = T.nextToken();
-		String major = T.nextToken();
-		Profile profile = makeNewProfile(name, major);
-		int noCredits = Integer.valueOf(T.nextToken());
+		String name= null;
+		String major = null;
+		Profile profile = new Profile();
+		
+		try {
+			name = T.nextToken();
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		try {
+			major = T.nextToken().trim();
+			profile = makeNewProfile(name, major);
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+			
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		
+		int noCredits = 0;
+		try {
+			noCredits = Integer.valueOf(T.nextToken().trim());
+		} 
+		catch (NumberFormatException nfe){
+			System.out.println("invalid credit hours");
+			return null;
+		} catch (NoSuchElementException nse){
+			System.out.println("invalid credit hours");
+			return null;
+		}
+		if (noCredits < 0) {
+			System.out.println("Credit hours cannot be negative.");
+		}
+		if (noCredits < 3) {
+			System.out.println("Minimum credit hours is 3.");
+			return null;
+		}
+		if (noCredits < 12) {
+			System.out.println("International students must enroll in at least 12 credits.");
+			return null;
+		}
+		
 		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
 		double tuitionDue = 0;
-		boolean isStudyAbroad = Boolean.valueOf(T.nextToken());
+		
+		boolean isStudyAbroad;
+		try {
+			isStudyAbroad = Boolean.valueOf(T.nextToken());
+		} catch(NoSuchElementException e) {
+			System.out.println("Missing whether international student is study abroad");
+			return null;
+		}
 		International newInternational = new International(profile, isFullTime, noCredits, tuitionDue, isStudyAbroad);
 		newInternational.setTuitionDue(newInternational.getTuitionDue());
 		newInternational.tuitionDue();
@@ -206,10 +375,55 @@ public class TuitionManager {
 	@author Emily Nelson
 	*/
 	public Tristate makeNewTristate(String line, StringTokenizer T) {
-		String name = T.nextToken();
-		String major = T.nextToken();
-		Profile profile = makeNewProfile(name, major);
-		int noCredits = Integer.valueOf(T.nextToken());
+		String name= null;
+		String major = null;
+		Profile profile = new Profile();
+		
+		try {
+			name = T.nextToken();
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		try {
+			major = T.nextToken().trim();
+			profile = makeNewProfile(name, major);
+		} catch (NoSuchElementException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+			
+		} catch (NullPointerException e) {
+			System.out.println("Missing data in command line.");
+			return null;
+		}
+		
+		
+		int noCredits = 0;
+		try {
+			noCredits = Integer.valueOf(T.nextToken().trim());
+		} 
+		catch (NumberFormatException nfe){
+			System.out.println("invalid credit hours");
+			return null;
+		} catch (NoSuchElementException nse){
+			System.out.println("invalid credit hours");
+			return null;
+		}
+		if (noCredits < 0) {
+			System.out.println("Credit hours cannot be negative.");
+		}
+		if (noCredits < 3) {
+			System.out.println("Minimum credit hours is 3.");
+			return null;
+		}
+		if (noCredits > 24) {
+			System.out.println("Credit hours exceed the maximum 24.");
+			return null;
+		}
 		boolean isFullTime = (noCredits < MINFULLTIME) ? false : true;
 		double tuitionDue = 0;
 		String state = T.nextToken().toUpperCase();
@@ -231,7 +445,15 @@ public class TuitionManager {
 	public Profile makeNewProfile(String name, String strMajor) {
 		Profile profile = new Profile();
 		String strMajorCap = strMajor.toUpperCase();
-		Major major = Major.valueOf(strMajorCap);
+		Major major = null;
+		try 
+		{
+			major = Major.valueOf(strMajorCap);
+		
+		} 
+		catch (IllegalArgumentException e) {
+			System.out.println(strMajor + " is not a valid major");
+		}
 		profile.setName(name);
 		profile.setMajor(major);
 		return profile;
@@ -244,15 +466,33 @@ public class TuitionManager {
 	@return instance of Profile
 	@author Emily Nelson
 	*/
-	public Profile makeNewProfile2(String line, StringTokenizer T) {
+	public Profile makeNewProfileFromCommand(String line, StringTokenizer T) {
 		String name = T.nextToken();
 		String strMajor = T.nextToken().toUpperCase();
-		Major major = Major.valueOf(strMajor);
+		Major major = null;
+		try 
+		{
+			major = Major.valueOf(strMajor);
+		
+		} 
+		catch (IllegalArgumentException e) {
+			System.out.println(strMajor + " is not a valid major");
+		}
 		Profile profile = new Profile();
 		profile.setName(name);
 		profile.setMajor(major);
 		return profile;
 		
+	}
+	
+	public void addStudent(Student student, boolean worked, Roster roster) {
+		   worked = roster.add(student);
+		   if (worked == true) {
+			   System.out.println("Student added");
+		   }
+		   else {
+			   System.out.println("Student already in roster");
+		   }
 	}
 	
 
